d55f333e346086be99c9aa76c82877a73e21deca
Harvey Xu <martinxuhui@gmail.com>
2013-07-02 22:34:10 +0200

    Add SpeakText to  support text to voice

--- src/com/wordpress/mariaanko/voicemaster/Translator.java
+++ src/com/wordpress/mariaanko/voicemaster/Translator.java
@@ -9,49 +9,55 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Locale;
+
+import android.app.Activity;
+import android.speech.tts.TextToSpeech;
 
 public class Translator {
 	private String YandexKey = "trnsl.1.1.20130630T095040Z.585df14183944045.6bd3edcc178f35bfc1993ce148b1d397234247a3";
-	
-	public String TranslateText(String sourceText, String sourceLanguageCode, String targetLanguageCode) throws IOException{
+	private TextToSpeech mTextToSpeech = null;
+	private int supported;
+
+	public String TranslateText(String sourceText, String sourceLanguageCode,
+			String targetLanguageCode) throws IOException {
 		String request = "https://translate.yandex.net/api/v1.5/tr.json/translate?key=";
-		request = request + YandexKey + "&lang=" + sourceLanguageCode + "-" + targetLanguageCode + "&text" + sourceText;
+		request = request + YandexKey + "&lang=" + sourceLanguageCode + "-"
+				+ targetLanguageCode + "&text" + sourceText;
 		URL url;
 		url = new URL(request);
 		HttpURLConnection connection = (HttpURLConnection) url.openConnection();
 		connection.setDoOutput(true);
 		connection.setConnectTimeout(60000);
 		connection.setUseCaches(false);
-		
+
 		BufferedReader in = new BufferedReader(new InputStreamReader(
 				connection.getInputStream()));
 		String translatedString;
 		while ((translatedString = in.readLine()) != null) {
 			System.out.println(translatedString);
 		}
-		
+
 		return null;
-		
+
 	}
-	
-	
-	public String GetTextOfSpeech(String pathOfSpeechFile, String language) throws IOException
-	{	    
-	    File file = new File(pathOfSpeechFile);
-	    if (!file.canRead())
-	    {
-	    	return null;
-	    }
-	    
-	    int fileLength = (int)file.length();
-	    byte [] speechData = new byte[fileLength];
-	    FileInputStream fis = new FileInputStream(file);
-	    fis.read(speechData);
-	    
-		String request = "https://www.google.com/"+
-                "speech-api/v1/recognize?"+
-                "xjerr=1&client=speech2text&lang=" + language + "&maxresults=10";
-		
+
+	public String GetTextOfSpeech(String pathOfSpeechFile, String language)
+			throws IOException {
+		File file = new File(pathOfSpeechFile);
+		if (!file.canRead()) {
+			return null;
+		}
+
+		int fileLength = (int) file.length();
+		byte[] speechData = new byte[fileLength];
+		FileInputStream fis = new FileInputStream(file);
+		fis.read(speechData);
+
+		String request = "https://www.google.com/" + "speech-api/v1/recognize?"
+				+ "xjerr=1&client=speech2text&lang=" + language
+				+ "&maxresults=10";
+
 		URL url;
 		url = new URL(request);
 		HttpURLConnection connection = (HttpURLConnection) url.openConnection();
@@ -64,7 +70,7 @@ public class Translator {
 		connection.setRequestProperty("User-Agent", "speech2text");
 		connection.setConnectTimeout(60000);
 		connection.setUseCaches(false);
-	       
+
 		DataOutputStream wr = new DataOutputStream(connection.getOutputStream());
 		wr.write(speechData);
 		wr.flush();
@@ -78,9 +84,31 @@ public class Translator {
 		while ((decodedString = in.readLine()) != null) {
 			System.out.println(decodedString);
 		}
-		
+
 		connection.disconnect();
 		return null;
-	} 
+	}
+
+	public int SpeakText(String text, final Locale language,
+			Activity currentContext) {
+
+		mTextToSpeech = new TextToSpeech(currentContext,
+				new TextToSpeech.OnInitListener() {
+
+					@Override
+					public void onInit(int status) {
+						// TODO Auto-generated method stub
+						supported = mTextToSpeech.setLanguage(language);
+					}
+				});
+
+		if ((supported != TextToSpeech.LANG_AVAILABLE)
+				&& (supported != TextToSpeech.LANG_COUNTRY_AVAILABLE)) {
+			return supported;
+		}
+
+		mTextToSpeech.speak(text, TextToSpeech.QUEUE_FLUSH, null);
+		return 0;
+	}
 
 }
